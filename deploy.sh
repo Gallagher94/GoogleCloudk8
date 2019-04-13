c4244b942c62cf573cfac50ea9950311272137eb
Gallagher94 <ruairidhgallaghers3@gmail.com>
2019-04-13 14:54:32 +0100

    Added new details for deployments file

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t gallagher94/multi-client:latest -t gallagher94/multi-client:$GIT_SHA -f ./client/Dockerfile ./client
-docker build -t gallagher94/multi-server:latest -t gallagher94/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t gallagher94/multi-client:latest -t gallagher94/multi-client:$GIT_SHA  -f ./client/Dockerfile ./client
+docker build -t gallagher94/multi-server:latest -t stephengrider/multi-server:$GIT_SHA  -f ./server/Dockerfile ./server
 docker build -t gallagher94/multi-worker:latest -t gallagher94/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
 
-docker push gallagher94/mulit-client:latest
-docker push gallagher94/mulit-server:latest
-docker push gallagher94/mulit-worker:latest
+docker push gallagher94/multi-client:latest
+docker push gallagher94/multi-server:latest
+docker push gallagher94/multi-worker:latest
 
-docker push gallagher94/mulit-client:$GIT_SHA
-docker push gallagher94/mulit-server:$GIT_SHA
-docker push gallagher94/mulit-worker:$GIT_SHA
+docker push gallagher94/multi-client:$GIT_SHA 
+docker push gallagher94/multi-server:$GIT_SHA 
+docker push gallagher94/multi-worker:$GIT_SHA 
 
 kubectl apply -f k8s
-kubectl set image deployment/server-deployment server=gallagher94/multi-server:$GIT_SHA
-kubectl set image deployment/client-deployment client=gallagher94/multi-client:$GIT_SHA
-kubectl set image deployment/worker-deployment worker=gallagher94/multi-worker:$GIT_SHA
+kubectl set image deployments/server-deployment server=gallagher94/multi-server:$GIT_SHA 
+kubectl set image deployments/client-deployment client=gallagher94/multi-client:$GIT_SHA 
+kubectl set image deployments/worker-deployment worker=gallagher94/multi-worker:$GIT_SHA 
